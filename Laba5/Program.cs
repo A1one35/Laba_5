d5ab788197dd18f1beecf3d4b6d539fd2e23a3b1
A1one35 <58321685+A1one35@users.noreply.github.com>
2020-06-05 19:11:49 +0300

    Second init Laba5

--- Laba5/Program.cs
+++ Laba5/Program.cs
@@ -9,39 +9,94 @@ namespace Laba5
 {
     class Program
     {
-        public string surName;
-        public string firstName;
-        public string patronymic;
-        public char sex;
-        public string dateOfBirth;
-        public char mathematicsMark;
-        public char physicsMark;
-        public char informaticsMark;
-        public int scholarship;
-        static Student[] ReadData(string fileName)
+        struct Student
         {
-            // TODO   implement this method.
-            // It should read the file whose fileName has been passed and fill 
-            fileName = File.ReadAllText("input.txt");
-        }
-        public Student(string lineWithAllData)
-        {
-            // TODO   you SHOULD IMPLEMENT constructor with exactly this signature
-            // lineWithAllData is string contating all data about one student, as described in statement
-            for (int i = 0; i < ; i++)
+            public string surName;
+            public string firstName;
+            public string patronymic;
+            public char sex;
+            public string dateOfBirth;
+            public char mathematicsMark;
+            public char physicsMark;
+            public char informaticsMark;
+            public int scholarship;
+            public Student(string lineWithAllData)
             {
-
+                string[] linesWithDataAboutOneStudent = lineWithAllData.Split(' ');
+                this.surName = linesWithDataAboutOneStudent[0];
+                this.firstName = linesWithDataAboutOneStudent[1];
+                this.patronymic = linesWithDataAboutOneStudent[2];
+                this.sex = Convert.ToChar(linesWithDataAboutOneStudent[3]);
+                this.dateOfBirth = linesWithDataAboutOneStudent[4];
+                this.mathematicsMark = Convert.ToChar(linesWithDataAboutOneStudent[5]);
+                this.physicsMark = Convert.ToChar(linesWithDataAboutOneStudent[6]);
+                this.informaticsMark = Convert.ToChar(linesWithDataAboutOneStudent[7]);
+                this.scholarship = Convert.ToInt32(linesWithDataAboutOneStudent[8]);
             }
-        }
-        static void runMenu(Student[] studs)
-        {
-            // TODO   implement this method
-            // It should call method(s) for concrete variant(s)
         }
         static void Main(string[] args)
         {
-            Student[] studs = ReadData("input.txt");
+            string path = @"input.txt";
+            Student[] studs = ReadData(path);
             runMenu(studs);
+            Console.ReadKey();
+        }
+        static void runMenu(Student[] studs)
+        {
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].sex == 'M')
+                {
+                    if ((studs[i].mathematicsMark == '2' && studs[i].physicsMark == '2') || (studs[i].mathematicsMark == '2' && studs[i].physicsMark == '1') || (studs[i].mathematicsMark == '2' && studs[i].physicsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].mathematicsMark == '1' && studs[i].physicsMark == '2') || (studs[i].mathematicsMark == '1' && studs[i].physicsMark == '1') || (studs[i].mathematicsMark == '1' && studs[i].physicsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].mathematicsMark == 45 && studs[i].physicsMark == '2') || (studs[i].mathematicsMark == 45 && studs[i].physicsMark == '1') || (studs[i].mathematicsMark == 45 && studs[i].physicsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+
+                    if ((studs[i].physicsMark == '2' && studs[i].informaticsMark == '2') || (studs[i].physicsMark == '2' && studs[i].informaticsMark == '1') || (studs[i].physicsMark == '2' && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].physicsMark == '1' && studs[i].informaticsMark == '2') || (studs[i].physicsMark == '1' && studs[i].informaticsMark == '1') || (studs[i].physicsMark == '1' && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].physicsMark == 45 && studs[i].informaticsMark == '2') || (studs[i].physicsMark == 45 && studs[i].informaticsMark == '1') || (studs[i].physicsMark == 45 && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+
+                    if ((studs[i].mathematicsMark == '2' && studs[i].informaticsMark == '2') || (studs[i].mathematicsMark == '2' && studs[i].informaticsMark == '1') || (studs[i].mathematicsMark == '2' && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].mathematicsMark == '1' && studs[i].informaticsMark == '2') || (studs[i].mathematicsMark == '1' && studs[i].informaticsMark == '1') || (studs[i].mathematicsMark == '1' && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                    if ((studs[i].mathematicsMark == 45 && studs[i].informaticsMark == '2') || (studs[i].mathematicsMark == 45 && studs[i].informaticsMark == '1') || (studs[i].mathematicsMark == 45 && studs[i].informaticsMark == 45))
+                    {
+                        Console.WriteLine("Більше двох екзаменів не здав -" + studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                }
+            }
+        }
+        static Student[] ReadData(string fileName)
+        {
+            string[] pathForFile = File.ReadAllLines(fileName);
+            Student[] ArrayOfStudent = new Student[pathForFile.Length];
+            for (int i = 0; i < pathForFile.Length; i++)
+            {
+                ArrayOfStudent[i] = new Student(pathForFile[i]);
+            }
+            return ArrayOfStudent;
         }
     }
 }
